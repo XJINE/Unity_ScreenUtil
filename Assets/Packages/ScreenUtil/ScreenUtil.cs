4799c3d6c53f645b8703897ebf1a5498645236cf
XJINE <umeda@team-lab.com>
2022-06-24 18:02:23 +0900

    Refactor ScreenUtil.

--- Assets/Packages/ScreenUtil/ScreenUtil.cs
+++ Assets/Packages/ScreenUtil/ScreenUtil.cs
@@ -4,35 +4,49 @@ public static class ScreenUtil
 {
     #region Property
 
-    public static float AspectRatio
-    {
-        get { return (float)Screen.width / Screen.height; }
-    }
+    public static float AspectRatio => (float)Screen.width / Screen.height;
 
     #endregion Property
 
     #region Method
 
-    public static Vector2Int ViewportToScreen(Vector2 coord)
+    public static Vector2Int ViewportToScreen(in Vector2 viewport, in bool normalize = false)
     {
-        return ScreenUtil.ViewportToScreen(coord.x, coord.y);
+        return ViewportToScreen(viewport.x, viewport.y, normalize);
     }
 
-    public static Vector2Int ViewportToScreen(float coordX, float coordY)
+    public static Vector2Int ViewportToScreen(in float viewportX, in float viewportY, bool normalize = false)
     {
-        return new Vector2Int((int)(Screen.width  * coordX),
-                              (int)(Screen.height * coordY));
+        return normalize ? new Vector2Int((int)Mathf.Lerp(0, Screen.width,  viewportX),
+                                          (int)Mathf.Lerp(0, Screen.height, viewportY))
+                         : new Vector2Int((int)(Screen.width  * viewportX),
+                                          (int)(Screen.height * viewportY));
     }
 
-    public static Vector2 ScreenToViewport(Vector2Int coord)
+    public static Vector2 ScreenToViewport(in Vector2Int screen, in bool normalize = false)
     {
-        return ScreenUtil.ScreenToViewport(coord.x, coord.y);
+        return ScreenToViewport(screen.x, screen.y, normalize);
     }
 
-    public static Vector2 ScreenToViewport(int coordX, int coordY)
+    public static Vector2 ScreenToViewport(in Vector3 screen, in bool normalize = false)
     {
-        return new Vector2((float)coordX / Screen.width,
-                           (float)coordY / Screen.height);
+        // NOTE:
+        // Input.mousePosition returns Vector3 type value.
+
+        return ScreenToViewport((int)screen.x, (int)screen.y, normalize);
+    }
+
+    public static Vector2 ScreenToViewport(in int screenX, in int screenY, in bool normalize = false)
+    {
+        var viewport = new Vector2((float) screenX / Screen.width, (float) screenY / Screen.height);
+
+        if (normalize)
+        {
+            viewport.x = Mathf.Lerp(0, 1, viewport.x);
+            viewport.y = Mathf.Lerp(0, 1, viewport.y);
+        }
+
+        return viewport;
     }
 
     #endregion Method
